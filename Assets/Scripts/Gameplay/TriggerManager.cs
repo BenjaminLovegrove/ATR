509f493587e67d583204ff87bd0c95d8975f8ea5
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-12-11 23:17:09 +1000

    Fog change triggers

--- Assets/Scripts/Gameplay/TriggerManager.cs
+++ Assets/Scripts/Gameplay/TriggerManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityStandardAssets.ImageEffects;
 using System.Collections;
 
 public class TriggerManager : MonoBehaviour {
@@ -10,26 +11,57 @@ public class TriggerManager : MonoBehaviour {
     public bool Memory;
     public AudioClip memoryDialogue;
 
+    [Header("Fog Change")]
+    public bool fogChange;
+    public float targFog;
+    private GlobalFog fog;
+    private float startFog;
+    private float fogLerp = 1;
+
     [Header("Enemy Patrol Start Trigger")]
     public GameObject enemy;
 
 	[Header("Checkpoint")]
 	public bool checkpoint;
 
+    void Start()
+    {
+        fog = GameObject.Find("Player").GetComponent<GlobalFog>();
+    }
 
     void OnTriggerEnter (Collider col) {
 	    if (col.gameObject.tag == "Player" && !triggered)
         {
 			triggered = true;
 
+            //Memory
             if (Memory)
             {
                 col.BroadcastMessage("EnterMemory");
             }
 
+            //Checkpoint
 			if (checkpoint){
 				EventManager.inst.currentCheckPoint ++;
 			}
+
+            //Fog
+            if (fogChange)
+            {
+                startFog = fog.heightDensity;
+                fogLerp = 0;
+            }
         }
 	}
+
+    void Update()
+    {
+        //Change fog
+        if (fogLerp < 1)
+        {
+            fogLerp += Time.deltaTime / 2;
+            fog.heightDensity = Mathf.Lerp(startFog, targFog, fogLerp);
+        }
+
+    }
 }
