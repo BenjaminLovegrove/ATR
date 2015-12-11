6a6da2c72eda3480bc79f578dabf055ec1b94a28
huuxxx <hdowling87@hotmail.com>
2015-12-12 01:10:28 +1000

    Added enemy activation trigger to trigger manager

--- Assets/Scripts/Gameplay/TriggerManager.cs
+++ Assets/Scripts/Gameplay/TriggerManager.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TriggerManager : MonoBehaviour {
+// Script to handle trigger events that can occur throughout the game
+// Set appropriate bools on the corresponding game object
+
+public class TriggerManager : MonoBehaviour
+{
 
     [Header("Trigger Settings")]
     bool triggered = false;
@@ -11,13 +15,15 @@ public class TriggerManager : MonoBehaviour {
     public AudioClip memoryDialogue;
 
     [Header("Enemy Patrol Start Trigger")]
+    public bool enemyTrigger;
     public GameObject enemy;
 
 	[Header("Checkpoint")]
 	public bool checkpoint;
 
 
-    void OnTriggerEnter (Collider col) {
+    void OnTriggerEnter (Collider col)
+    {
 	    if (col.gameObject.tag == "Player" && !triggered)
         {
 			triggered = true;
@@ -30,6 +36,11 @@ public class TriggerManager : MonoBehaviour {
 			if (checkpoint){
 				EventManager.inst.currentCheckPoint ++;
 			}
+
+            if (enemyTrigger)
+            {
+                enemy.SetActive(true);
+            }
         }
 	}
 }
