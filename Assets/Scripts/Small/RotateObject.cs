f26bd951f27ceafdd5b37b9aee89dce2acd3d1e7
huuxxx <hdowling87@hotmail.com>
2016-03-17 22:53:19 +1000

    Main Menu Props

--- Assets/Scripts/Small/RotateObject.cs
+++ Assets/Scripts/Small/RotateObject.cs
@@ -5,9 +5,28 @@ public class RotateObject : MonoBehaviour
 {
     public GameObject rotatingObj;
     public int rotationSpeed;
+
+    public bool rotateZ;
+    public bool rotateY;
+    public bool rotateX;
+
 	
 	void FixedUpdate ()
     {
-        rotatingObj.transform.Rotate(Vector3.back * Time.deltaTime * rotationSpeed);
+        if (rotateZ)
+        {
+            rotatingObj.transform.Rotate(Vector3.back * Time.deltaTime * rotationSpeed);
+        }
+
+        if (rotateY)
+        {
+            rotatingObj.transform.Rotate(Vector3.down * Time.deltaTime * rotationSpeed);
+        }
+
+        if (rotateX)
+        {
+            rotatingObj.transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+        }
+        
 	}
 }
