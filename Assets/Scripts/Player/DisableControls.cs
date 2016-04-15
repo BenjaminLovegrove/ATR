65ac80d6eb8bfa5a80a73368d1b9581014284e90
huuxxx <hdowling87@hotmail.com>
2016-04-15 10:58:49 +1000

    Disable Controls Function Properly
    Now works properly across all interactions

---
+++ Assets/Scripts/Player/DisableControls.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableControls : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Destroy(this);
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            EventManager.inst.controlsDisabled = true;
+        }
+    }
+}
