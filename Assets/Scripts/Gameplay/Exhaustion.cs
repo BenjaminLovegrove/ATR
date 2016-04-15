5c77e88ce73a0d50e1ec6135b43884a459c8a9a8
Benjamin <Benjamin_JL@live.com>
2016-04-15 14:34:28 +1000

    Exhaustion in coast ending

---
+++ Assets/Scripts/Gameplay/Exhaustion.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exhaustion : MonoBehaviour {
+
+    public AudioSource breathing;
+    public float totalDistance;
+    public float maxVol;
+    public Transform endPoint;
+    private float closestDist;
+   
+    void Start()
+    {
+        closestDist = totalDistance;
+    }
+
+	void Update () {
+        if (Vector3.Distance (transform.position, endPoint.position) < closestDist)
+        {
+            closestDist = Vector3.Distance(transform.position, endPoint.position);
+        }
+
+        print(Vector3.Distance(transform.position, endPoint.position));
+
+        breathing.volume = Mathf.Lerp(maxVol, 0, (closestDist / totalDistance));
+	}
+}
