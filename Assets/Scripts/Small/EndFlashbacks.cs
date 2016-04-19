121a52e758ce19190ede5707345e3b80052049c1
huuxxx <hdowling87@hotmail.com>
2016-04-19 20:41:10 +1000

    Hill Climb Flashbacks

---
+++ Assets/Scripts/Small/EndFlashbacks.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EndFlashbacks : MonoBehaviour
+{
+    public AudioClip flashbackClip;
+    private AudioSource flashbackSFX;
+    private Image flashObject;
+    private bool triggered;
+
+    public IEnumerator FlashbackCo()
+    {
+        flashObject.CrossFadeAlpha(155, 2f, false);
+        yield return new WaitForSeconds(2f);
+        flashObject.CrossFadeAlpha(0f, 2f, false);
+        yield return new WaitForSeconds(flashbackClip.length);
+        Destroy(this.gameObject);
+    }
+
+	void Awake ()
+    {
+        triggered = false;
+        flashbackSFX = GameObject.Find("FlashbackSFX").GetComponent<AudioSource>();
+        flashObject = GameObject.Find("MemoryFlashObj").GetComponent<Image>();
+	}
+	
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player" && !triggered)
+        {
+            triggered = true;
+            StartCoroutine("FlashbackCo");
+            if (flashbackClip != null)
+            {
+                flashbackSFX.clip = flashbackClip;
+                flashbackSFX.Play();
+            }
+            else print("Missing flashback SFX");
+        }
+    }
+}
