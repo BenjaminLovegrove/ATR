11b74671c47fc9fae0764833abb3d91fbf20cc7d
Benjamin <Benjamin_JL@live.com>
2016-04-22 09:28:58 +1000

    Added audio switcher to first scene on bgmsource

---
+++ Assets/Scripts/Small/PlaceholderMusicSwitch.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceholderMusicSwitch : MonoBehaviour {
+
+    public AudioSource bgmSource;
+    public AudioClip track0;
+    public AudioClip track1;
+    private int trackNum;
+    private float switchDelay;
+
+	void Update () {
+	    if (!bgmSource.isPlaying)
+        {
+            switchDelay += Time.deltaTime;
+        }
+
+        if (!bgmSource.isPlaying && switchDelay > 12f)
+        {
+            if (trackNum == 0)
+            {
+                bgmSource.clip = track1;
+                trackNum = 1;
+            } else
+            {
+                bgmSource.clip = track0;
+                trackNum = 0;
+            }
+
+            bgmSource.Play();
+            switchDelay = 0;
+        }
+	}
+}
