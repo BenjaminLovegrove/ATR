9c13907ea0a2e975f8fa79927421855f6371928a
huuxxx <hdowling87@hotmail.com>
2016-04-26 18:42:48 +1000

    Updated Credits

--- Assets/Scripts/UI/Credits.cs
+++ Assets/Scripts/UI/Credits.cs
@@ -1,81 +1,175 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 // Script for the UI used in credits scene
 
 public class Credits : MonoBehaviour
 {
-    public GameObject atr;
-    public GameObject ben;
-    public GameObject hux;
-    public GameObject jacko;
-    public GameObject jess;
-    public GameObject voiceActors;
-    public GameObject team;
-    public AudioClip musicClip;
+    [Header("ATR")]
+    public Text atr;
     
+    [Header("Ben")]
+    public Text benHeading;
+    public Text benRole0;
+    public Text benRole1;
+    public Text benWeb;
+    
+    [Header("Hux")]
+    public Text huxHeading;
+    public Text huxRole0;
+    public Text huxRole1;
+    public Text huxWeb;
+    
+    [Header("Jack")]
+    public Text jackHeading;
+    public Text jackRole0;
+    public Text jackRole1;
+    public Text jackRole2;
+   
+    [Header("Jess")]
+    public Text jessHeading;
+    public Text jessRole0;
+    public Text jessRole1;
+    public Text jessRole2;
+   
+    [Header("Voice Actors")]
+    public Text voiceHeading;
+    public Text adrian;
+    public Text christy;
+    public Text tony;
+   
+    [Header("Team")]
+    public Text teamHeading;
+    
+    [Header("Containers")]
+    public Text[] textObjs;
+    public Image[] imageObjs;
+    public int[] order;                 // Set elements to 4 (Inspector)
+    public bool[] sequenceTriggered;    // Set elements to 6 (Inspector)
+
     private float autoExit;
     private float timer;
     private List<int> creditsOrder = new List<int>();
-    public int[] order;
-    public bool[] sequenceTriggered;    
+  
 
-    // ATR
+    // ATR 4 - 4secs
     public IEnumerator ATR()
     {
-        yield return new WaitForSeconds(1f);
-        atr.SetActive(true);
+        atr.CrossFadeAlpha(1, 0.25f, false);
         yield return new WaitForSeconds(3f);
-        atr.SetActive(false);
+        atr.CrossFadeAlpha(0, 1f, false);
+        yield return new WaitForSeconds(1f);
     }
 
-    // BENNY
+    // BENNY - 10secs
     public IEnumerator Sequence1()
     {
-        ben.SetActive(true);
-        yield return new WaitForSeconds(14f);
-        ben.SetActive(false);
+        benHeading.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(1f);
+        benRole0.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(0.5f);
+        benRole1.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(0.5f);
+        benWeb.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(7f);
+
+        //Fade out
+        benHeading.CrossFadeAlpha(0, 1, false);
+        benRole0.CrossFadeAlpha(0, 1, false);
+        benRole1.CrossFadeAlpha(0, 1, false);
+        benWeb.CrossFadeAlpha(0, 1, false);
+        yield return new WaitForSeconds(1f);
     }
     
-    // HUX
+    // HUX - 10secs
     public IEnumerator Sequence2()
     {
-        hux.SetActive(true);
-        yield return new WaitForSeconds(14f);
-        hux.SetActive(false);
+        huxHeading.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(1f);
+        huxRole0.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(0.5f);
+        huxRole1.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(0.5f);
+        huxWeb.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(7f);
+
+        //Fade out
+        huxHeading.CrossFadeAlpha(0, 1, false);
+        huxRole0.CrossFadeAlpha(0, 1, false);
+        huxRole1.CrossFadeAlpha(0, 1, false);
+        huxWeb.CrossFadeAlpha(0, 1, false);
+        yield return new WaitForSeconds(1f);
     }
     
-    // JACKO
+    // JACKO - 10secs
     public IEnumerator Sequence3()
     {
-        jacko.SetActive(true);
-        yield return new WaitForSeconds(14f);
-        jacko.SetActive(false);
+        jackHeading.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(1f);
+        jackRole0.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(0.5f);
+        jackRole1.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(0.5f);
+        jackRole2.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(7f);
+
+        //Fade out
+        jackHeading.CrossFadeAlpha(0, 1, false);
+        jackRole0.CrossFadeAlpha(0, 1, false);
+        jackRole1.CrossFadeAlpha(0, 1, false);
+        jackRole2.CrossFadeAlpha(0, 1, false);
+        yield return new WaitForSeconds(1f);
     }
     
-    // JESS
+    // JESS - 10secs
     public IEnumerator Sequence4()
     {
-        jess.SetActive(true);
-        yield return new WaitForSeconds(14f);
-        jess.SetActive(false);
+        jessHeading.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(1f);
+        jessRole0.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(0.5f);
+        jessRole1.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(0.5f);
+        jessRole2.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(7f);
+
+        //Fade out
+        jessHeading.CrossFadeAlpha(0, 1, false);
+        jessRole0.CrossFadeAlpha(0, 1, false);
+        jessRole1.CrossFadeAlpha(0, 1, false);
+        jessRole2.CrossFadeAlpha(0, 1, false);
+        yield return new WaitForSeconds(1f);
     }
 
-    // VOICE ACTORS
+    // VOICE ACTORS - 10secs
     public IEnumerator Sequence5()
     {
-        voiceActors.SetActive(true);
-        yield return new WaitForSeconds(9f);
-        voiceActors.SetActive(false);
+        voiceHeading.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(1f);
+        christy.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(0.5f);
+        tony.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(0.5f);
+        adrian.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(7f);
+
+        //Fade out
+        voiceHeading.CrossFadeAlpha(0, 1, false);
+        adrian.CrossFadeAlpha(0, 1, false);
+        christy.CrossFadeAlpha(0, 1, false);
+        tony.CrossFadeAlpha(0, 1, false);
+        yield return new WaitForSeconds(1f);
     }
 
-    // TEAM
+    // TEAM - 8secs
     public IEnumerator Sequence6()
     {
-        team.SetActive(true);
-        yield return new WaitForSeconds(7.5f);
-        team.SetActive(false);
+        teamHeading.CrossFadeAlpha(1, 1, false);
+        yield return new WaitForSeconds(7f);
+        teamHeading.CrossFadeAlpha(0, 1, false);
+        yield return new WaitForSeconds(1f);
     }
 
     void Awake()
@@ -85,7 +179,7 @@ public class Credits : MonoBehaviour
 
     void Start()
     {
-        autoExit = musicClip.length;
+        autoExit = 64;
         StartCoroutine("ATR");
 
         // Reset vals to normal
@@ -126,6 +220,16 @@ public class Credits : MonoBehaviour
         {
             sequenceTriggered[k] = false;
         }
+
+        for (int l = 0; l < textObjs.Length; l++)
+        {
+            textObjs[l].CrossFadeAlpha(0, 0, false);
+        }
+
+        for (int m = 0; m < imageObjs.Length; m++)
+        {
+            imageObjs[m].CrossFadeAlpha(0, 0, false);
+        }
     }
 
     void SequenceCalls()
@@ -138,35 +242,35 @@ public class Credits : MonoBehaviour
         }
 
         // Second team member
-        if (timer > 20 && !sequenceTriggered[1])
+        if (timer > 15 && !sequenceTriggered[1])
         {
             sequenceTriggered[1] = true;
             StartCoroutine("Sequence" + order[1]);
         }
 
         // Third team member
-        if (timer > 35 && !sequenceTriggered[2])
+        if (timer > 25 && !sequenceTriggered[2])
         {
             sequenceTriggered[2] = true;
             StartCoroutine("Sequence" + order[2]);
         }
 
         // Fourth team member
-        if (timer > 50 && !sequenceTriggered[3])
+        if (timer > 35 && !sequenceTriggered[3])
         {
             sequenceTriggered[3] = true;
             StartCoroutine("Sequence" + order[3]);
         }
 
         // Voice actors
-        if (timer > 65 && !sequenceTriggered[4])
+        if (timer > 45 && !sequenceTriggered[4])
         {
             sequenceTriggered[4] = true;
             StartCoroutine("Sequence5");
         }
 
-        // Voice actors
-        if (timer > 75 && !sequenceTriggered[5])
+        // Team
+        if (timer > 55 && !sequenceTriggered[5])
         {
             sequenceTriggered[5] = true;
             StartCoroutine("Sequence6");
