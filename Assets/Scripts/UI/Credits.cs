5437cff704347c5c1c24daad1e2c476392ca8b7b
huuxxx <hdowling87@hotmail.com>
2016-04-26 19:58:41 +1000

    Credits updated - Hux

--- Assets/Scripts/UI/Credits.cs
+++ Assets/Scripts/UI/Credits.cs
@@ -21,6 +21,11 @@ public class Credits : MonoBehaviour
     public Text huxRole0;
     public Text huxRole1;
     public Text huxWeb;
+    public Text huxCodeLeft;
+    public Text huxCodeRight;
+    public GameObject huxCodeLeftObj;
+    public GameObject huxCodeRightObj;
+    private bool huxActive;
     
     [Header("Jack")]
     public Text jackHeading;
@@ -93,14 +98,41 @@ public class Credits : MonoBehaviour
         huxRole1.CrossFadeAlpha(1, 1, false);
         yield return new WaitForSeconds(0.5f);
         huxWeb.CrossFadeAlpha(1, 1, false);
-        yield return new WaitForSeconds(7f);
+        yield return new WaitForSeconds(1f);
+        huxCodeLeft.text = BinaryCodeGenerator();
+        huxCodeRight.text = BinaryCodeGenerator();
+        huxCodeLeft.CrossFadeAlpha(1, 0, false);
+        huxCodeRight.CrossFadeAlpha(1, 0, false);
+
+        huxActive = true;
+        yield return new WaitForSeconds(1f);
+        huxCodeLeft.text = BinaryCodeGenerator();
+        huxCodeRight.text = BinaryCodeGenerator();
+        yield return new WaitForSeconds(1f);
+        huxCodeLeft.text = BinaryCodeGenerator();
+        huxCodeRight.text = BinaryCodeGenerator();
+        yield return new WaitForSeconds(1f);
+        huxCodeLeft.text = BinaryCodeGenerator();
+        huxCodeRight.text = BinaryCodeGenerator();
+        yield return new WaitForSeconds(1f);
+        huxCodeLeft.text = BinaryCodeGenerator();
+        huxCodeRight.text = BinaryCodeGenerator();
+        yield return new WaitForSeconds(1f);
+        huxCodeLeft.text = BinaryCodeGenerator();
+        huxCodeRight.text = BinaryCodeGenerator();
+        yield return new WaitForSeconds(1f);
+        huxCodeLeft.text = BinaryCodeGenerator();
+        huxCodeRight.text = BinaryCodeGenerator();
 
         //Fade out
         huxHeading.CrossFadeAlpha(0, 1, false);
         huxRole0.CrossFadeAlpha(0, 1, false);
         huxRole1.CrossFadeAlpha(0, 1, false);
         huxWeb.CrossFadeAlpha(0, 1, false);
+        huxCodeLeft.CrossFadeAlpha(0, 0, false);
+        huxCodeRight.CrossFadeAlpha(0, 0, false);
         yield return new WaitForSeconds(1f);
+        huxActive = false;
     }
     
     // JACKO - 10secs
@@ -194,6 +226,12 @@ public class Credits : MonoBehaviour
 
     void Update()
     {
+        if (huxActive)
+        {
+            huxCodeLeftObj.gameObject.transform.Translate(Vector3.down * Time.deltaTime * 30);
+            huxCodeRightObj.gameObject.transform.Translate(Vector3.up * Time.deltaTime * 30);
+        }
+
         timer += Time.deltaTime;
 
         SequenceCalls();
@@ -288,5 +326,16 @@ public class Credits : MonoBehaviour
         int temp = creditsOrder[rand];
         creditsOrder.RemoveAt(rand);
         return temp;
+    }
+
+    string BinaryCodeGenerator()
+    {
+        string temp = "";
+        for (int i = 0; i < 3000; i++)
+        {
+            int j = Random.Range(0, 2);
+            temp = temp + j;
+        }
+            return temp;
     }
 }
