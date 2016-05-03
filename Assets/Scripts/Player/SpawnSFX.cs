07c84808acf5f09ca9b05942355a96d80c5ee0b1
Benjamin <Benjamin_JL@live.com>
2016-05-03 12:08:20 +1000

    Respawn inhale, icon

---
+++ Assets/Scripts/Player/SpawnSFX.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnSFX : MonoBehaviour {
+
+    public AudioSource spawnSFX;
+    public AudioSource ambientBreathing;
+    public float ambientDelay;
+    private bool started;
+
+	void Start () {
+	    
+	}
+	
+	void Update () {
+        ambientDelay -= Time.deltaTime;
+
+        if (ambientDelay < 0 && !ambientBreathing.isPlaying && !started)
+        {
+            ambientBreathing.Play();
+        }
+	}
+}
